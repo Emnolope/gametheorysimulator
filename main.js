dc10789c79a87d916af5c4a2eadada73914adaac
Emnolope <44859532+Emnolope@users.noreply.github.com>
2020-07-09 16:34:41 -0700

    *

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
+//WORLD
 chairs = new class chairs {
-    seats={};
-    addagent(agent) {
-        console.log(agent);
-        for (var other in this.seats) {
-            console.log("hi");
-            this.seats[other].brain[agent.name]=0;
-            agent.brain[this.seats[other].name]=0;
-        }
-        this.seats[agent.name]=agent;
+  seats={};
+  addagent(agent) {
+    console.log(agent);
+    for (var other in this.seats) {
+      console.log("hi");
+      this.seats[other].brain[agent.name]=0;
+      agent.brain[this.seats[other].name]=0;
     }
-    killagent(name){
-        for (var other in this.seats) {
-            if (other==name) continue;
-            this.seats[other].brain.delete(name);
-        }
-        seats.delete(name)
+    this.seats[agent.name]=agent;
+  }
+  killagent(name){
+    for (var other in this.seats) {
+      if (other==name) continue;
+      this.seats[other].brain.delete(name);
     }
+    seats.delete(name)
+  }
 }()
 
 id=0;
@@ -23,27 +24,36 @@ class agent {
   brain={};
   stomach=0;
   strat=null;
-  record=function(name,action) {
-    this.brain[name]+=action*2-1;
-  }
+  record=null;
   name=null;
   constructor(strat) {
     if (strat!==undefined) {
-        this.strat=strat;
+      this.record=strat.think;
+      this.strat=strat.act;
     }
     this.name=id++;
   }
 }
 
 var strategy={};
-strategy.ccc=function(mem) {
-  return 1;
+strategy.ccc={
+  think:function(name,action) {
+    this.brain[name]+=action*2-1;
+  },
+  act:function(mem) {
+    return 1;
+  }
 }
-strategy.ddd=function(mem) {
-  return 0;
+strategy.ddd={
+  think:function(name,action) {
+    this.brain[name]+=action*2-1;
+  },
+  act:function(mem) {
+    return 0;
+  }
 }
 
-payoff=[[[-1,-1],[-1,1]],[[1,-1],[1,1]]]
+payoff=[[-1,1],[-1,1]]//]
 
 function vs(a,b){
   ago=a.strat(a.brain[b.name]);
@@ -53,15 +63,16 @@ function vs(a,b){
   a.stomach+=aget;
   b.stomach+=bget;
   a.record(b.name,bgo)
-  b.record(b.name,bgo)
+  b.record(a.name,ago)
 }
 
 //chairs.addagent(new agent());
-chairs.addagent(new agent(strategy.ddd));
 chairs.addagent(new agent(strategy.ccc));
-vs(mean, nice)
+chairs.addagent(new agent(strategy.ccc));
+//vs(mean, nice)
+vs(chairs.seats[0],chairs.seats[1])
 //
 //
 // I was just about to test the vs function with ddd and ccc, and check the brains of the objects to see if they are functioning correctly .
 //
-//
+//always defect tit 4 tat tit 4 tat suspicious spiteful tit 4 tat integral tit 4 tat integral but defects ddc ccd cd ccc ddd
